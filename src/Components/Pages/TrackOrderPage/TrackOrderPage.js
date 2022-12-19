a78574e5bd43332e73f6486b286a713b4107217b
dwikis17 <dwikis17@gmail.com>
2022-12-19 20:52:10 +0700

    add trackorder fuunctionality

--- src/Components/Pages/TrackOrderPage/TrackOrderPage.js
+++ src/Components/Pages/TrackOrderPage/TrackOrderPage.js
@@ -1,16 +1,84 @@
-import { Input } from 'antd';
-import React from 'react';
+import { Button, Input, Table } from 'antd';
+import { isEmpty } from 'lodash';
+import React, { useState } from 'react';
+import { GET_TRANSACTION_BY_ORDER_ID, PAYMENT_REDIRECT } from '../../../Constants/Apis';
+import { useAxios } from '../../../Utils/CommonUtils/useAxios';
 import Header from '../../Header/Header';
 
+const { Search } = Input;
 function TrackOrderPage(props) {
+  const [searchedOrderId, setSearchedOrderId] = useState('');
+  const { fetchedData: { data }, callReFetch } = useAxios(`${GET_TRANSACTION_BY_ORDER_ID}/${searchedOrderId}`);
+
+  const renderPayButton = (token) => {
+    return (
+      <Button type="submit" className="text-white bg-red-700 w-full" onClick={() => window.open(`${PAYMENT_REDIRECT}/${token}`)}>
+        Bayar
+      </Button>
+    );
+  };
+  const renderItems = (items, record) => {
+    return items === 'Pending' ? renderPayButton(record?.transactionToken) : null;
+  };
+
+  const handleSearch = (value) => {
+    setSearchedOrderId(value);
+    callReFetch();
+  };
+
+  const columns = [
+    {
+      title: 'Order ID',
+      dataIndex: 'orderId',
+      key: 'orderId',
+    },
+    {
+      title: 'Game',
+      dataIndex: 'game',
+      key: 'game',
+    },
+    {
+      title: 'In Game Name',
+      dataIndex: 'inGameName',
+      key: 'inGameName',
+    },
+    {
+      title: 'Tag Line',
+      dataIndex: 'tagLine',
+      key: 'tagLine',
+    },
+    {
+      title: 'Email',
+      dataIndex: 'email',
+      key: 'email',
+    },
+    {
+      title: 'Payment Status',
+      dataIndex: 'paymentStatus',
+      key: 'paymentStatus',
+    },
+    {
+      title: 'Action',
+      dataIndex: 'paymentStatus',
+      render: (items, record) => renderItems(items, record)
+    }
+  ];
   return (
     <div className="container justify-items-center grid">
       <Header />
       <h1 className=" text-4xl text-white mt-10">Track pesanan anda </h1>
-      <div className="grid grid-row-2 w-full p-10 justify-items-center">
-        <Input className="w-1/2" placeholder="Masukan transaksi ID disini" />
+      <div className="grid  w-full p-10 justify-items-center">
+        <Search className="sm:w-3/4 w-full" placeholder="Masukan transaksi ID disini" enterButton onSearch={(event) => handleSearch(event)} />
       </div>
-      <div className="grid grid-row-2 w-1/2 border h-96 p-10 justify-items-center" />
+      <div className="grid w-full borde gap-0 justify-items-center">
+        {
+          data
+          && <Table columns={columns} dataSource={[data]} pagination={false} />
+        }
+        {
+          isEmpty(data) && !isEmpty(searchedOrderId) && <h1 className="text-white">Order Id Tidak ditemukan !</h1>
+        }
+      </div>
     </div>
   );
 }
--- src/Constants/Apis.js
+++ src/Constants/Apis.js
@@ -2,5 +2,6 @@ const baseUrl = 'http://localhost:80/api';
 
 export const VERIFY_TOKEN_API = `${baseUrl}/user/verify`;
 export const FETCH_ALL_GAMES_API = `${baseUrl}/game`;
-export const PAYMENT_API = `${baseUrl}/payment`;
+export const PAYMENT_API = `${baseUrl}/transaction`;
+export const GET_TRANSACTION_BY_ORDER_ID = `${baseUrl}/transaction/get-transaction`;
 export const PAYMENT_REDIRECT = 'https://app.sandbox.midtrans.com/snap/v2/vtweb';
