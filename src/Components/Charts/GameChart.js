6d7ee921aa3a5c80bf7f5ee97950134104f20d2c
dwikis17 <dwikis17@gmail.com>
2023-01-21 19:00:19 +0700

    apex chart refactor

--- src/Components/Charts/GameChart.js
+++ src/Components/Charts/GameChart.js
@@ -1,8 +1,11 @@
+import { DatePicker, MonthPicker } from 'antd';
 import React from 'react';
 import Chart from 'react-apexcharts';
+import dayjs from 'dayjs';
 
-function GameChart({ data }) {
-  console.log(data);
+function GameChart({
+  data, state, dispatch, callReFetch
+}) {
   const chartConfig = {
     options: {
       chart: {
@@ -13,10 +16,26 @@ function GameChart({ data }) {
     series: data?.totalTransaction
   };
 
+  const onChange = (date, dateString) => {
+    dispatch({ type: 'month', payload: date.$d.getMonth() + 1 });
+    dispatch({ type: 'year', payload: date.$d.getFullYear() });
+    callReFetch();
+  };
+
   return (
     <div className="app">
       <div className="row">
         <div className="mixed-chart">
+          <DatePicker onChange={onChange} picker="month" defaultValue={dayjs(`${state.year}-${state.month}`, 'YYYY-M')} />
+          <h1 className="text-2xl text-white">
+            Perbandingan penjualan per-product pada tahun
+            {' '}
+            {state.year}
+            {' '}
+            bulan
+            {' '}
+            {state.month}
+          </h1>
           <Chart
             options={chartConfig.options}
             series={chartConfig.series}
